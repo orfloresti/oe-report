5b3b3b1fbd31f3cfbe275ea54ca9eb21c0c7dcc9
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-10-31 12:22:02 -0600

    MVP with call to api to get data

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Basic application in react + vite to generate a report of activities completed on OpenEnglish.
 
+## Get data
+
+- Filter network by `details?personId`
+- Cookie and data should be set in config
--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,50 @@ import Form from "./components/Form/Form";
 import Card from "./components/card/Card";
 
 function App() {
-  const [result, setResult] = useState();
-  const handleSubmit = (form) => {
-    console.log(form);
-  }
+  const [report, setReport] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (form) => {
+    setReport(null)
+    setLoading(true);
+    const response = await fetch("http://localhost:3000/", {
+      body: JSON.stringify(form),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+
+    setReport(response);
+    setLoading(false);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <Card title="Open English Report">
-          <Form onSubmit={handleSubmit}/>
+          <Form onSubmit={handleSubmit} />
         </Card>
-        {result ? <Card title="Results"></Card> : null}
+        {loading && (
+          <Card title="Results">
+            <p>Loading ...</p>
+          </Card>
+        )}
+        {report && (
+          <Card title="Results">
+            <>
+              <p> Time: {(report.time.hours).toFixed(2)}h </p>
+              <p> Open English - {report.weeks} </p>
+              <ul>
+                {Object.keys(report.activities).map((activity) => (
+                  <li key={activity}>
+                    {`${activity}: ${report.activities[activity]}`}
+                  </li>
+                ))}
+              </ul>
+            </>
+          </Card>
+        )}
       </div>
     </div>
   );
